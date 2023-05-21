90bd395f3fb3ce24832e3978236fc64ed1488eb6
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-05-21 18:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+	greet(){
+		console.log(`Hello, my name is ${name}, I am ${age} years old`);
+		
+	}
+}
 
-function Employee(name, age, jobTitle) {}
-
+function Employee(name, age, jobTitle) {
+Person.call(this, name,age);
+this.jobtitle=jobtitle;
+	jobGreet(){
+		console.log(`Hello, my name is ${name},Iam ${age} years old, and my job title is ${jobtitle}` );
+	}
+}
+Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.constructor=Employee;
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
